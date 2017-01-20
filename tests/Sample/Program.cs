07c84a4e517608455a69ea7c662dc62cb9283ff4
cuiliang <cuiliang.bj@gmail.com>
2017-01-20 10:34:21 +0800

    更改目录，增加sample项目。

---
+++ tests/Sample/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading.Tasks;
+using Sample;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        MainAsync(args).GetAwaiter().GetResult();
+
+        Console.ReadKey();
+    }
+
+    public static async Task MainAsync(string[] args)
+    {
+        try
+        {
+            await NewSdkTester.RunAsync();
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Ex:" + ex.Message);
+            throw;
+        }
+
+
+    }
+}
