1c96db86aa96d7e0e4ab75fb5c13563e54d018d6
cuiliang <cuiliang.bj@gmail.com>
2018-05-21 13:47:02 +0800

    getobject增加返回metadata。

--- src/Api/Object/Get/GetObjectCommand.cs
+++ src/Api/Object/Get/GetObjectCommand.cs
@@ -1,8 +1,14 @@
-﻿using System.Net.Http;
+﻿using System;
+using System.Diagnostics;
+using System.Globalization;
+using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
 using Cuiliang.AliyunOssSdk.Api.Base;
+using Cuiliang.AliyunOssSdk.Api.Common.Consts;
 using Cuiliang.AliyunOssSdk.Entites;
 using Cuiliang.AliyunOssSdk.Request;
+using Cuiliang.AliyunOssSdk.Utility;
 
 namespace Cuiliang.AliyunOssSdk.Api.Object.Get
 {
@@ -43,12 +49,47 @@ namespace Cuiliang.AliyunOssSdk.Api.Object.Get
             var result = new GetObjectResult();
             result.Headers = response.Headers;
             result.Content = response.Content;
+            result.Metadata = Deserialize(response);
 
             return new OssResult<GetObjectResult>()
             {
                 IsSuccess = true,
                 SuccessResult = result
             };
+        }
+
+        private ObjectMetadata Deserialize(HttpResponseMessage response)
+        {
+            
+            var metadata = new ObjectMetadata();
+            foreach (var header in response.Headers)
+            {
+                if (header.Key.StartsWith(OssHeaders.OssUserMetaPrefix, false, CultureInfo.InvariantCulture))
+                {
+                    // The key of user in the metadata should not contain the prefix.
+                    metadata.UserMetadata.Add(header.Key.Substring(OssHeaders.OssUserMetaPrefix.Length),
+                        header.Value.FirstOrDefault());
+                }
+                else if (string.Equals(header.Key, HttpHeaders.ContentLength, StringComparison.InvariantCultureIgnoreCase))
+                {
+                    // Content-Length. Parse should not fail.
+                    metadata.ContentLength = long.Parse(header.Value.FirstOrDefault(), CultureInfo.InvariantCulture);
+                }
+                else if (string.Equals(header.Key, HttpHeaders.ETag, StringComparison.InvariantCultureIgnoreCase))
+                {
+                    metadata.ETag = OssUtils.TrimETag(header.Value.FirstOrDefault());
+                }
+                else if (string.Equals(header.Key, HttpHeaders.LastModified, StringComparison.InvariantCultureIgnoreCase))
+                {
+                    metadata.LastModified = DateUtils.ParseRfc822Date(header.Value.FirstOrDefault());
+                }
+                else
+                {
+                    // Treat the other headers just as strings.
+                    metadata.AddHeader(header.Key, header.Value);
+                }
+            }
+            return metadata;
         }
     }
 }
