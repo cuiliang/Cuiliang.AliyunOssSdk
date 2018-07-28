3f115d5ac5cb90ef0c650a69456fe345763d161c
dudu <dudu@cnblogs.com>
2018-07-28 15:22:49 +0800

    Fix build failures

--- src/OssClient.cs
+++ src/OssClient.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
 using Cuiliang.AliyunOssSdk.Api;
--- src/Request/ServiceCaller.cs
+++ src/Request/ServiceCaller.cs
@@ -16,7 +16,6 @@ namespace Cuiliang.AliyunOssSdk.Request
     /// </summary>
     public class ServiceCaller
     {
-        private readonly static HttpClient _client = new HttpClient();
         private RequestContext _requestContext;
         private readonly HttpClient _client;
 
