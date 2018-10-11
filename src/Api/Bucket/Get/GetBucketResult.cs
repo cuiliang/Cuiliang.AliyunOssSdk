5549afcd7ed14bc83b14a849af2eb7d25f70f9bb
cuiliang <cuiliang.bj@gmail.com>
2018-10-11 23:05:42 +0800

    save

--- src/Api/Bucket/Get/GetBucketResult.cs
+++ src/Api/Bucket/Get/GetBucketResult.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 using System.Xml.Serialization;
+using Cuiliang.AliyunOssSdk.Api.Common;
 
 namespace Cuiliang.AliyunOssSdk.Api.Bucket.Get
 {
@@ -56,7 +57,7 @@ namespace Cuiliang.AliyunOssSdk.Api.Bucket.Get
         /// <summary>
         /// 保存Bucket拥有者信息的容器。
         /// </summary>
-        public OwnerClass Owner
+        public Owner Owner
         {
             get; set;
         }
@@ -66,60 +67,49 @@ namespace Cuiliang.AliyunOssSdk.Api.Bucket.Get
         /// </summary>
         public string Prefix { get; set; }
 
+        
+
+    }
+
+    /// <summary>
+    /// Content里的每个元素，保存每个Object对象的信息。
+    /// </summary>
+    [XmlRoot("Contents")]
+    public class ObjectMeta
+    {
         /// <summary>
-        /// Content里的每个元素，保存每个Object对象的信息。
+        /// 保存Bucket拥有者信息的容器。
         /// </summary>
-        [XmlRoot("Contents")]
-        public class ObjectMeta
+        [XmlElement("Owner")]
+        public Owner Owner
         {
-            /// <summary>
-            /// 保存Bucket拥有者信息的容器。
-            /// </summary>
-            [XmlElement("Owner")]
-            public OwnerClass Owner
-            {
-                get; set;
-            }
-
-            /// <summary>
-            /// ETag (entity tag) 在每个Object生成的时候被创建，用于标示一个Object的内容。对于Put Object请求创建的Object，ETag值是其内容的MD5值；对于其他方式创建的Object，ETag值是其内容的UUID。ETag值可以用于检查Object内容是否发生变化。不建议用户使用ETag来作为Object内容的MD5校验数据完整性。
-            /// </summary>
-            public string ETag { get; set; }
-
-            /// <summary>
-            /// Object的Key.
-            /// </summary>
-            public string Key { get; set; }
-
-            /// <summary>
-            /// Object最后被修改的时间。
-            /// </summary>
-            public string LastModified { get; set; }
-
-
-            /// <summary>
-            /// Object的字节数。
-            /// </summary>
-            public string Size { get; set; }
-
-            /// <summary>
-            /// Object的存储类型，目前只能是“Standard”类。
-            /// </summary>
-            public string StorageClass { get; set; }
+            get; set;
         }
 
-        [XmlRoot("Owner")]
-        public class OwnerClass
-        {
-            /// <summary>
-            /// Object 拥有者的名字。
-            /// </summary>
-            public string DisplayName { get; set; }
+        /// <summary>
+        /// ETag (entity tag) 在每个Object生成的时候被创建，用于标示一个Object的内容。对于Put Object请求创建的Object，ETag值是其内容的MD5值；对于其他方式创建的Object，ETag值是其内容的UUID。ETag值可以用于检查Object内容是否发生变化。不建议用户使用ETag来作为Object内容的MD5校验数据完整性。
+        /// </summary>
+        public string ETag { get; set; }
 
-            /// <summary>
-            /// Bucket拥有者的用户ID。
-            /// </summary>
-            public string ID { get; set; }
-        }
+        /// <summary>
+        /// Object的Key.
+        /// </summary>
+        public string Key { get; set; }
+
+        /// <summary>
+        /// Object最后被修改的时间。
+        /// </summary>
+        public string LastModified { get; set; }
+
+
+        /// <summary>
+        /// Object的字节数。
+        /// </summary>
+        public string Size { get; set; }
+
+        /// <summary>
+        /// Object的存储类型，目前只能是“Standard”类。
+        /// </summary>
+        public string StorageClass { get; set; }
     }
 }
